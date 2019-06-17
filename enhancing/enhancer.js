f93c74d145cd54ef4d84274fd505eb953c86f8df
anthony <stackchain@gmail.com>
2019-06-17 16:27:11 -0400

    commit - mistake. repair done, not succeed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,7 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item, durability: 100 };
+  return { ...item };
 }
 
 function fail(item) {
@@ -14,7 +14,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -2,18 +2,18 @@ const { succeed, fail, repair } = require('./enhancer.js');
 // test away!
 
 describe('enhancer.js', () => {
-    describe('succeed()', () => {
+    describe('repair()', () => {
         it('restores durability to 100', () => {
-            expect(succeed({ durability: 89 }).durability).toBe(100);
-            expect(succeed({ durability: -89 }).durability).toBe(100);
-            expect(succeed({ durability: 100 }).durability).toBe(100);
-            expect(succeed({ durability: 0 })).toEqual({ durability: 100 });
+            expect(repair({ durability: 89 }).durability).toBe(100);
+            expect(repair({ durability: -89 }).durability).toBe(100);
+            expect(repair({ durability: 100 }).durability).toBe(100);
+            expect(repair({ durability: 0 })).toEqual({ durability: 100 });
         });
     });
 
     describe('success()', () => { });
 
     it('should have a max of 100 for durability', () => {
-        expect(succeed({ durability: 101 }).durability).toBe(100);
+        expect(repair({ durability: 101 }).durability).toBe(100);
     });
 });
